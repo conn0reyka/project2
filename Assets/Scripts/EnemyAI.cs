87416c6fbb6b83ab2700c2123a3d35a011e1d48c
PIPAPUPA77 <saveliy.mazeev@gmail.com>
2024-02-25 20:09:22 +0300

    added abilty to chase player on sight for mobs

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,7 +6,11 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+    public PlayerController player;
     public List<Transform> patrolPoints;
+    public float viewAngle;
 
     void Start()
     {
@@ -14,9 +18,10 @@ public class EnemyAI : MonoBehaviour
         PickNewAgentPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        NoticePLayerUpdate();
+        ChaseUpdate();
         patrolUpdate();
     }
 
@@ -27,9 +32,12 @@ public class EnemyAI : MonoBehaviour
 
     private void patrolUpdate()
     {
-    if(_navMeshAgent.remainingDistance == 0)
+        if(!_isPlayerNoticed)
         {
-            PickNewAgentPoint();
+            if(_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewAgentPoint();
+            }
         }
     }
 
@@ -38,4 +46,31 @@ public class EnemyAI : MonoBehaviour
         _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
+    private void NoticePLayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if(hit.collider.gameObject == player.gameObject)
+                {
+                     _isPlayerNoticed = true;
+                }
+                
+            }
+        }
+    }
+
+    private void ChaseUpdate()
+    {
+        if(_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+
 }
+
