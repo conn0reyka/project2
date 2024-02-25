f1d6af02d7fe3472a0b06469047000753489f675
PIPAPUPA77 <saveliy.mazeev@gmail.com>
2024-02-25 18:28:36 +0300

    added patroling ability

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    public List<Transform> patrolPoints;
+
+    void Start()
+    {
+        InitComponentLinks();
+        PickNewAgentPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        patrolUpdate();
+    }
+
+    private void PickNewAgentPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void patrolUpdate()
+    {
+    if(_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewAgentPoint();
+        }
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+}
