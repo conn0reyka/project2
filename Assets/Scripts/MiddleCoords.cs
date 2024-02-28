2282d80596643c94a9839d64a62b98779651932c
PIPAPUPA77 <saveliy.mazeev@gmail.com>
2024-02-28 18:38:31 +0300

    testing things

---
+++ Assets/Scripts/MiddleCoords.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleCoords : MonoBehaviour
+{
+    public Transform Point1;
+    public Transform Point2;
+    
+    void Update()
+    {
+        transform.position = Vector3.Lerp(Point1.position, Point2.position, 0.5f);
+    }
+}
